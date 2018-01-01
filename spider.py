0163dfd4e955ab75188ba01780a78d5d0fa236b7
SHIU Chun Ming <hugoshiu1222@gmail.com>
2018-01-01 12:04:32 +0800

    m

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import os
 import requests
 import time
 import re
@@ -14,13 +15,14 @@ def extractURL():
 
     for link in soup.find_all('a', {"class": "posLink", "href": re.compile("(/job/)")}):
         if link.attrs['href'] not in urls:
-            urls.add(str(link.attrs['href']))
+            urls.add(download(str(link.attrs['href'])))
 
     return urls
 
 def download(url):
-    file_name = "data/urls/" + "" + str(url).split('/')[-1]
-    # print(file_name)
+    file_name = "data/urls/" + "" + url.split('/')[-1] + ".html"
+    print(file_name)
+
     try:
         web = requests.get(url)
     except:
@@ -32,7 +34,10 @@ def download(url):
 
     with open(file_name, mode='wb') as file:
         content = web.text
+
         file.write(content.encode(web.encoding))
+    
+    return file_name
 
 if __name__ == '__main__':
 
@@ -43,16 +48,20 @@ if __name__ == '__main__':
     except:
         with open("log/errorlog", mode='a') as file:
             logging.exception("ERROR: Network/HTMLStatus encounter en error.")
-            file.write("time: " + time.asctime(time.localtime(time.time())))
-            file.write("\nBaseURL: " + url)
             sys.exit(1)
 
     with open("information-technology.html", mode='wb') as file:
         content = web.text
         file.write(content.encode(web.encoding))
     
+    for file in os.listdir("data/urls"):
+        os.remove("data/urls/" + file)
+
     urls = extractURL()
     for url in urls:
-        download(url)
+        soup = BeautifulSoup(open(url), "html.parser")
+        for item in soup.find_all('div', {'class': 'jobad-primary'}):
+            if re.search(re.compile("(?i)C\+\+|Python|Java|trading|cloud computing|big data"), item.text.rstrip().replace(" ", "")):
+                print(url) 
     
     
